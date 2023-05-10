0b75801dda36fea76f05b0465b49fef29d9d3104
Gazihan Alankuş <gazialankus@gmail.com>
2023-05-10 17:22:45 +0300

    Initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:se380_student/firebase_options.dart';
 import 'package:se380_student/ui/students_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ProviderScope(
       child: MyApp(),
